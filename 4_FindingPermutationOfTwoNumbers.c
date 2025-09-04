e85243744f8d33d47bc1e08f4893bb00c47d491e
1234abhibisht <abhibisht129@gmail.com>
2025-09-04 19:02:28 +0530

    Create 4_FindingPermutationOfTwoNumbers.c

---
+++ 4_FindingPermutationOfTwoNumbers.c
@@ -0,0 +1,23 @@
+/* nPr = n!/(n-r)!*/
+#include <stdio.h>
+int factorial(int a) {
+    int mul = 1;
+    for(int i = a; i >= 1; i--) {
+        mul = mul * i;
+    }
+    return mul;
+}
+int Permutation(int n, int r) {
+    int x = factorial(n)/factorial(n-r);
+    printf("nPr = %d",x);
+}
+int main() {
+    int n;
+    printf("Enter total number of entities : ");
+    scanf("%d",&n);
+    int r;
+    printf("Enter number of items to be selected : ");
+    scanf("%d",&r);
+    Permutation(n,r);
+    return 0;
+}
