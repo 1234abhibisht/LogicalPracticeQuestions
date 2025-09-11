052df63179a529bb861dc18bfbf0fe2c992c4473
1234abhibisht <abhibisht129@gmail.com>
2025-09-11 20:06:27 +0530

    Create 9_FindingPrimeFactorsOfANumber.c

---
+++ 9_FindingPrimeFactorsOfANumber.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int primenumber(int p) {
+   for(int i = 2; i <= p-1; i++) {
+        if(p % i == 0) {  /* composite number */
+            return 0;
+        }
+        else {  /* prime number */
+            return 1;
+        }
+   } 
+   return 1;
+}
+void factor(int n) {
+    int a = 0;
+    for(int i = 2; i <= n; i++) {
+        if(n % i == 0 && primenumber(i)) {
+                printf("%d ",i);
+            }
+        }
+    }
+int main() {
+    int n,p;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    factor(n);
+    return 0;
+}
