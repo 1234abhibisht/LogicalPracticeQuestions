3ae93b7ff2113c347ec72d7211de3b28813c84d5
1234abhibisht <abhibisht129@gmail.com>
2025-09-11 19:55:42 +0530

    Update 3_FindingCombinationOfTwoNumbers.c

--- 3_FindingCombinationOfTwoNumbers.c
+++ 3_FindingCombinationOfTwoNumbers.c
@@ -9,7 +9,7 @@ int fact(int a) {  /* this function is for factoral loop */
     return mul;
 }
 int Combination(int n, int r) {   /* this function is for using loop in formula */
-    int x = fact(n);
+    int x = fact(n);  /* here I have use fact(n) because I want to take factorial of n not of a variable */
     int y = fact(r) * fact(n-r);
     printf("Combination of %d and %d is : %d",n, r, x/y);
 }
