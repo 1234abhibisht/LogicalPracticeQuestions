5ad6b23a3d253c4910a91b1823615ddababb2a09
1234abhibisht <abhibisht129@gmail.com>
2025-09-14 23:19:47 +0530

    Update 24_1sAnd0sComplementOfABinaryNumber.c

--- 24_1sAnd0sComplementOfABinaryNumber.c
+++ 24_1sAnd0sComplementOfABinaryNumber.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
-int countnumber(int a) {
-    int count = 0;
-    while(a != 0) {
-        a = a / 10;
-        count++;
-    }
-    return count;
-}
-void complement(int arr[]) { 
-    for(int j = 0; j <= 2; j++) {
+void complement(int a,int arr[]) { 
+    for(int j = 0; j <= a - 1; j++) {
         if(arr[j] == 1) {
             arr[j] = 0;
         }
@@ -19,20 +11,14 @@ void complement(int arr[]) {
     }
 }
 int main() {
-    /* I have asked user a number of any digit and stored it in a variable named n*/
     int n;
-    printf("Enter a binary number : ");
+    printf("How many digits will be in your binary number : ");
     scanf("%d",&n);
-    /* now I have counted number of digits in the number and made an array having containers 
-       according to the number of digits */
-    int arr[countnumber(n)];
-    int lastdigit = 0;
-    /* then I have stored number entered in variable n in that array, each index having one element of that number*/
-    for(int i = countnumber(n) - 1; i >= 0; i--) {
-        lastdigit = n % 10;
-        arr[i] = lastdigit;
-        n = n / 10;
+    int arr[n];
+    printf("Enter a binary number with spaces between them : ");
+    for(int i = 0; i <= n - 1; i++) {
+        scanf("%d",&arr[i]);
     }
-    complement(arr);
+    complement(n,arr);
     return 0;
 }
