8ff8a1e77c85b5975daa60762ed80ba22a51abb5
1234abhibisht <abhibisht129@gmail.com>
2025-09-17 19:05:35 +0530

    Create 31_a_Power_b_UsingLoops.c

---
+++ 31_a_Power_b_UsingLoops.c
@@ -0,0 +1,20 @@
+/* a power b = a * a * a * a...b times */
+
+#include <stdio.h>
+void power(int a, int b) {
+    int mul = 1;
+    for(int i = 1; i <= b; i++) {
+        mul = mul * a;
+    }
+    printf("%d",mul);
+}
+int main() {
+    int a;
+    printf("Enter base value : ");
+    scanf("%d",&a);
+    int b;
+    printf("Enter power value : ");
+    scanf("%d",&b);
+    power(a,b);
+    return 0;
+}
