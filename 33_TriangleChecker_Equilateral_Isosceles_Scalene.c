7e2523c5921324f1b6c324135edf1ed00ef85fe7
1234abhibisht <abhibisht129@gmail.com>
2025-09-18 12:22:14 +0530

    Create 33_TriangleChecker_Equilateral_Isosceles_Scalene.c

---
+++ 33_TriangleChecker_Equilateral_Isosceles_Scalene.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void triangleCheck(int a, int b, int c) {
+    if(a + b > c && b + c > a && c + a > b) {
+        printf("Triangle is valid\n");
+        if(a == b && a == c) {
+        printf("Triangle is Equilateral");
+        }
+    else if((a == b && a != c) || (a == c && a != c) || (b == c && b != a)) {
+            printf("Triangle is Isosceles");
+        }
+    else if(a != b && a != c && b != c) {
+        printf("Triangle is Scalene");
+     }
+    }
+    else {
+        printf("Triangle is not valid");
+    }
+}
+int main() {
+    int a;
+    printf("Enter first length : ");
+    scanf("%d",&a);
+    int b;
+    printf("Enter second length : ");
+    scanf("%d",&b);
+    int c;
+    printf("Enter third length : ");
+    scanf("%d",&c);
+    triangleCheck(a,b,c);
+    return 0;
+}
