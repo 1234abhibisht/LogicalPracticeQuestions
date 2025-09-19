ba7637a640a0e3d01df341cbd9bc48c74b007e90
1234abhibisht <abhibisht129@gmail.com>
2025-09-19 20:02:34 +0530

    Update 02_GreatestNumberChecker.c

--- 02_GreatestNumberChecker.c
+++ 02_GreatestNumberChecker.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
-int greatest(int n, int num[]) {
+void greatest(int n, int num[])
+{
     int max = num[0];
-    for(int i = 1; i <= n-1; i++) {
-        if(num[i] > max) max = num[i];
+    for (int i = 1; i <= n - 1; i++)
+    {
+        if (num[i] > max)
+            max = num[i];
     }
-    printf("Greatest number is : %d",max);
-    return max;
+    printf("Greatest number is : %d", max);
+    return;
 }
-int main() {
+int main()
+{
     int n;
     printf("Enter how many numbers you want to compare : ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int num[n];
     printf("Enter your numbers : ");
-    for(int i = 0; i <= n-1; i++) {
-        scanf("%d",&num[i]);
+    for (int i = 0; i <= n - 1; i++)
+    {
+        scanf("%d", &num[i]);
     }
-    greatest(n,num);
-    return 0   ;
+    greatest(n, num);
+    return 0;
 }
