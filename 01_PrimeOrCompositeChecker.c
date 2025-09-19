a50a468017cdda5b710081dafa459f7b71c3bb5e
1234abhibisht <abhibisht129@gmail.com>
2025-09-19 20:01:33 +0530

    Update 01_PrimeOrCompositeChecker.c

--- 01_PrimeOrCompositeChecker.c
+++ 01_PrimeOrCompositeChecker.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
-int PrimeComposite(int n) {
+void PrimeComposite(int n)
+{
     int a = 0;
-    for(int i = 2; i <= n-1; i++) {
-        if(n%i == 0) a = 1;
-        else a = 0;
+    for (int i = 2; i <= n - 1; i++)
+    {
+        if (n % i == 0)
+            a = 1;
+        else
+            a = 0;
         break;
     }
-    if(n == 1) printf("%d is neither prime nor composite",n);
-    else if(a == 1) printf("%d is composite",n);
-    else printf("%d is prime",n);
+    if (n == 1)
+        printf("%d is neither prime nor composite", n);
+    else if (a == 1)
+        printf("%d is composite", n);
+    else
+        printf("%d is prime", n);
+    return;
 }
-int main() {
+int main()
+{
     int n;
     printf("Enter a number : ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     PrimeComposite(n);
     return 0;
 }
