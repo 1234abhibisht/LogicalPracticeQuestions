ffed8ffad3e47c0cae7ae53f872506f1ab2dbb24
1234abhibisht <abhibisht129@gmail.com>
2025-09-21 14:08:00 +0530

    Create 39_RamanujanNumberChecker.c

---
+++ 39_RamanujanNumberChecker.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{ 
+     /* limit is 20 */
+    /* (i)cube + (j)cube = n 
+       (k)cube + (l)cube = n
+       condition = i != k && j != l && i != l && j != k */
+       for(int i = 0; i <= 20; i++) {
+            for(int j = 0; j <= 20; j++) {
+               int a = pow(i,3) + pow(j,3);
+               for(int k = 0; k <= 20; k++) {
+                    for(int l = 0; l <= 20; l++) {
+                        int b = pow(k,3) + pow(l,3);
+                        if(a == b && i != k && i != l && j != k && j != l) {
+                            printf("%d\n",a);
+                        }
+                    }
+               }
+            }
+       }
+    return 0;
+}
