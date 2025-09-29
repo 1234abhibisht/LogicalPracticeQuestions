4caf170a91aa97d386738315461700a40f3cb2cb
1234abhibisht <abhibisht129@gmail.com>
2025-09-29 22:01:23 +0530

    Update 03_FindingCombinationOfTwoNumbers.c

--- 03_FindingCombinationOfTwoNumbers.c
+++ 03_FindingCombinationOfTwoNumbers.c
@@ -1,26 +1,30 @@
 /* ncr = n!/r!(n-r)! */
 /* ncr = n!/r!(n-r)! */
 #include <stdio.h>
-int fact(int a) {  /* this function is for factoral loop */
-   int mul = 1;
-    for(int i = a; i >= 1; i--) {
+int fact(int a)
+{ /* this function is for factoral loop */
+    int mul = 1;
+    for (int i = a; i >= 1; i--)
+    {
         mul = mul * i;
     }
     return mul;
 }
-int Combination(int n, int r) {   /* this function is for using loop in formula */
-    int x = fact(n);  /* here I have use fact(n) because I want to take factorial of n not of a variable */
-    int y = fact(r) * fact(n-r);
-    printf("Combination of %d and %d is : %d",n, r, x/y);
+int Combination(int n, int r)
+{                    /* this function is for using loop in formula */
+    int x = fact(n); /* here I have use fact(n) because I want to take factorial of n not of a variable */
+    int y = fact(r) * fact(n - r);
+    printf("Combination of %d and %d is : %d", n, r, x / y);
 }
-int main() {
+int main()
+{
     int n;
     printf("Enter number of items : ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int r;
     printf("Enter number of selected items from n: ");
-    scanf("%d",&r);
+    scanf("%d", &r);
     fact(n);
-    Combination(n,r);
+    Combination(n, r);
     return 0;
 }
