bfa4e7444959ca2801aef7f751c5a23c73524a24
1234abhibisht <abhibisht129@gmail.com>
2025-10-04 21:30:55 +0530

    Update 06_Finding_HCF_OfTwoNumbers.c

--- 06_Finding_HCF_OfTwoNumbers.c
+++ 06_Finding_HCF_OfTwoNumbers.c
@@ -1,30 +1,29 @@
 /* greatest common divisor or highest common factor */
-/* for hcf, first number should be greater than second number*/
 #include <stdio.h>
-int hcf(int firstnumber, int secondnumber)
-{
-    if (secondnumber == 0)
-    { /* base case */
-        return firstnumber;
-    }
-    int x = hcf(secondnumber, firstnumber % secondnumber);
+int minimum(int firstnumber, int secondnumber) {
+    int x;
+    if(firstnumber > secondnumber) x = firstnumber;
+    else x = secondnumber;
     return x;
 }
-int main()
-{
+int hcf(int firstnumber, int secondnumber) {
+    int hcfab;
+    /* start loop from end */
+    for(int i = minimum(firstnumber,secondnumber); i >= 1; i--) {
+        if(firstnumber % i == 0 && secondnumber % i == 0) {
+            hcfab = i;
+            break;
+        }
+    }
+    printf("Highest common factor is : %d",hcfab);
+}
+int main() {
     int firstnumber;
     printf("Enter firs tnumber : ");
-    scanf("%d", &firstnumber);
+    scanf("%d",&firstnumber);
     int secondnumber;
     printf("Enter second number : ");
-    scanf("%d", &secondnumber);
-    if (firstnumber < secondnumber)
-    {
-        /* swap both values */
-        int temp = firstnumber;
-        firstnumber = secondnumber;
-        secondnumber = temp;
-    }
-    printf("HCF(%d,%d) is %d", firstnumber, secondnumber, hcf(firstnumber, secondnumber));
+    scanf("%d",&secondnumber);
+    hcf(firstnumber,secondnumber);
     return 0;
 }
